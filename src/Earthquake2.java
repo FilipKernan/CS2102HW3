e8732eaf1423438adda6a8c23bb9196ce64a2b20
Unknown <pilotpatrick2000@gmail.com>
2018-11-15 11:13:54 -0500

    Comments and JavaDocs

--- src/Earthquake2.java
+++ src/Earthquake2.java
@@ -1,13 +1,5 @@
 import java.util.LinkedList;
 
-/**
- * Holy fucking shit this is some raw code. I feel bad for whoever has to go through this. At least it works (?)
- * But seriously. the dailyMaxForMonth METHOD takes in a list of sensor readings and a certain month (1-12) and returns list of MaxHzReports for that month
- * the data PARAMETER is a list of doubles that the sensor provided. Every so often is an 8-digit date followed by some values for that day
- * the month PARAMETER is just a month, 1-12
- * the RESULT, a LinkedList of MaxHzReports, is a list of dates and readings, where each date is within the given month and is followed by a single value.
- *      This value is the greatest value obtained by the sensor on that day
- */
 class Earthquake2 {
       
   // checks whether a datum is a date
@@ -15,6 +7,14 @@ class Earthquake2 {
   // extracts the month from an 8-digit date
   int extractMonth(double dateNum) { return ((int)dateNum % 10000) / 100; }
 
+    /**
+     * Holy fucking shit this is some raw code. I feel bad for whoever has to go through this. At least it works (?)
+     * But seriously. the dailyMaxForMonth METHOD takes in a list of sensor readings and a certain month (1-12) and returns list of MaxHzReports for that month
+     * @param data the data PARAMETER is a list of doubles that the sensor provided. Every so often is an 8-digit date followed by some values for that day
+     * @param month the month PARAMETER is just a month, 1-12
+     * @return the RESULT, a LinkedList of MaxHzReports, is a list of dates and readings, where each date is within the given month and is followed by a single value.
+     *      This value is the greatest value obtained by the sensor on that day
+     */
   public LinkedList<MaxHzReport> dailyMaxForMonth(LinkedList<Double> data, int month) {
       data.add(99999999.0);         //This makes sure the data for the last date is actually added to listOfLists (see if statement on line 17)
       LinkedList<LinkedList<Double>> listOfLists = new LinkedList<>();
--- src/EarthquakeExamples.java
+++ src/EarthquakeExamples.java
@@ -50,3 +50,22 @@ public class EarthquakeExamples {
     assertEquals(OctReports, E2.dailyMaxForMonth(threeDates, 10));
   }
 }
+
+/*
+Earthquake 1 takes the list of sensor data and first cleans out everything that doesn't
+    pertain to the given month. Then it goes through the cleaned list, storing the most
+    recent date and keeping track of the maximum HZ readout for that date. Once a new
+    date is encountered, the previous date and the stored maximum are added to the final
+    list called 'result,' which is what the method returns.
+
+Earthquake 2 takes the list of sensor data and turns it into a list of lists of doubles.
+    The initial data is separated by dates; each internal list is organized as an eight
+    digit date followed by the sensor readings for that date. Once another date is
+    encountered, a new embedded list is created. Once the list of lists is created,
+    it is iterated through by a for loop, which first checks if the first item in each
+    internal list, which will always be an 8-digit date, matches the given month. If it
+    does, the rest of that list is evaluated, and the highest value, along with the date,
+    is then turned into a MaxHzReport and added to the listOfMax, which is what the method
+    returns.
+
+ */
